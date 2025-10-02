2537891bd325555f8ab68c6d9004330c20da97aa
jlfmendes32 <jlfmendes32@gmail.com>
2025-10-02 12:00:39 +0100

    PLC2025

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,2 +1,52 @@
 import re
+import sys
+
+
+#Cabeçalho
+cabe=re.compile(r'(?P<cabeçalho>^(#*)\s*(.*))')
+im=re.compile(r'(?P<img>!\[(.*)\]\((.*)\))')
+bold=re.compile(r'(?P<bold>\*\*(.*)\*\*)')
+ita=re.compile(r'(?P<ita>\*(.*)\*)')
+lik=re.compile(r'(?P<link>\[(.*)\]\((.*)\))')
+ListaNum=re.compile(r'(?P<lista>((\d+\.)\s*(.+)+))')
+
+def converte(t):
+    i=0
+    nh=len(cabe.search(t).group(2))
+    t=re.sub(cabe,rf"<h{nh}>\g<3></h{nh}>",t)
+    t=re.sub(bold,r"<b>\2</b>",t)
+    t=re.sub(ita,r"<i>\2</i>",t)
+    t=re.sub(im,r'<img src="\3" alt="\2"/>',t)
+    t=re.sub(lik,r'<a href="\3">\2</a>',t)
+    t=re.split("\n",t)
+    tamanho=len(t)
+    while (i<tamanho):
+        if re.match(ListaNum,t[i]):
+            t[i-1]=t[i-1]+"\n<ol>"
+            while(re.match(ListaNum,t[i]) and i< tamanho):
+                t[i]=re.sub(ListaNum,r"<li>\4</li>",t[i])
+                i+=1
+            t[i]=t[i]+"</ol>\n"
+        else:    
+            i+=1
+
+    t="\n".join(t)
+    return t
+
+def read_input():
+    # if a filename was passed, read that file; otherwise read stdin
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        with open(path, 'r', encoding='utf-8') as f:
+            return f.read()
+    return sys.stdin.read()
+
+def main():
+    text = read_input()
+    out = converte(text)
+    sys.stdout.write(out)
+
+if __name__ == "__main__":
+    main()
+
 
