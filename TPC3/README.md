584041e99787faa721e76e40713eda5becb225ac
jlfmendes32 <jlfmendes32@gmail.com>
2025-10-07 16:07:31 +0100

    PLC2025

---
+++ TPC3/README.md
@@ -0,0 +1,12 @@
+### Analisador Léxico de Queery
+
+[Resolução](https://github.com/jlfmendes32/PLC2025/blob/main/TPC3/Res)
+
+Resumo:
+
+Utilizando as ferramentas disponibilizadas na aula teórica, configurou-se o 'json' para se identificar os tokens respetivos da linguaguem, sendo tudo compilado num ficheiro em python.
+
+
+João Mendes, pg60226
+
+<img src="https://github.com/user-attachments/assets/9eab7434-fb2c-43ef-bbc4-4d3f88e145fc" width="180">
---
+++ TPC3/Res/tpc3.py
@@ -0,0 +1,49 @@
+
+import sys
+import re
+
+def tokenize(input_string):
+    reconhecidos = []
+    linha = 1
+    mo = re.finditer(r'(?P<S>SELECT)|(?P<W>WHERE)|(?P<VAR>\?\w+)|(?P<ID>\w*:\w+)|(?P<PONTO>\.)|(?P<PA>\{)|(?P<PF>\})|(?P<SKIP>[ \t])|(?P<NEWLINE>\n)|(?P<ERRO>.)', input_string)
+    for m in mo:
+        dic = m.groupdict()
+        if dic['S']:
+            t = ("S", dic['S'], linha, m.span())
+
+        elif dic['W']:
+            t = ("W", dic['W'], linha, m.span())
+    
+        elif dic['VAR']:
+            t = ("VAR", dic['VAR'], linha, m.span())
+    
+        elif dic['ID']:
+            t = ("ID", dic['ID'], linha, m.span())
+    
+        elif dic['PONTO']:
+            t = ("PONTO", dic['PONTO'], linha, m.span())
+    
+        elif dic['PA']:
+            t = ("PA", dic['PA'], linha, m.span())
+    
+        elif dic['PF']:
+            t = ("PF", dic['PF'], linha, m.span())
+    
+        elif dic['SKIP']:
+            t = ("SKIP", dic['SKIP'], linha, m.span())
+    
+        elif dic['NEWLINE']:
+            t = ("NEWLINE", dic['NEWLINE'], linha, m.span())
+    
+        elif dic['ERRO']:
+            t = ("ERRO", dic['ERRO'], linha, m.span())
+    
+        else:
+            t = ("UNKNOWN", m.group(), linha, m.span())
+        if not dic['SKIP'] and t[0] != 'UNKNOWN': reconhecidos.append(t)
+    return reconhecidos
+
+for linha in sys.stdin:
+    for tok in tokenize(linha):
+        print(tok)    
+
